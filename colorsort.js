de0a7381e543c48595216d3ece19198a2ee2211e
Kelly Chinn <kpchinn9@gmail.com>
2015-03-02 13:15:20 -0800

    colorsort

---
+++ colorsort.js
@@ -0,0 +1,28 @@
+
+function shuffle(m) {
+  var rand, $rand;
+  rand=Math.floor(Math.random() * m--);
+  $('li:eq('+ m +')').
+  after($('li:eq('+ rand +')')).
+  insertBefore($('li:eq('+ rand +')'));
+
+  if(m) {
+    setTimeout(shuffle, 100, m);
+  }
+}
+
+var $colors = $('li');
+var basicSort = $colors.sort (function(a,b) {
+  return $(a).find("p").text() > $(b).find("p").text();
+});
+
+
+$('#shuffle').on('click', function() {
+  shuffle($('li:eq').length);
+});
+
+
+$('#sort').on('click', function(){
+  $('.wrapper').html(basicSort);
+});
+ 
